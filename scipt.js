006dc8e8cd69e98819864a2131212d84fe36d5d6
andys19a <andre.alm@gritacademy.se>
2024-04-12 13:49:32 +0200

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -1,4 +1,10 @@
-//kommentar nr2//
 
-// this is a comment//
+let time = 17;
 
+if(time > 15){
+    alert("it is not morning");
+}else if(time < 22){
+    alert("it is evening");
+}else{
+    alert("is it something else")
+}
